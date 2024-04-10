2add8083c7903a1d973791259ebda0b11722450c
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2024-04-09 21:27:07 -0300

    update header

--- app/main.js
+++ app/main.js
@@ -2,3 +2,12 @@
 //  queda el archivo para futuras mejoras
 //  se agrego la llamada en el tag onclick="window.modal.showModal()"
 //  y onclick="window.modal.close()"
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
