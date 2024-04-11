042328ecccd52ef147794325d193c3c434e40105
alizunega <alizunega84@gmail.com>
2024-04-11 10:52:40 -0300

    commit 11-04 media queries

--- app/main.js
+++ app/main.js
@@ -1,4 +1,19 @@
-//  por el momento no fue necesario usar js
-//  queda el archivo para futuras mejoras
-//  se agrego la llamada en el tag onclick="window.modal.showModal()"
-//  y onclick="window.modal.close()"
+/* comportamiento del scroll en dispositivos pequeños */
+document.addEventListener("DOMContentLoaded", function () {
+  // Detecta el desplazamiento del usuario y activa el desplazamiento suave
+  document
+    .querySelector(".container-items")
+    .addEventListener("scroll", function (event) {
+      // Verifica si el usuario está desplazándose hacia abajo
+      if (this.scrollTop > this.lastScrollTop) {
+        // Encuentra el siguiente elemento visible y lo desplaza al centro de la ventana
+        const nextItem = document.querySelector(
+          ".item:not(.scrolled):first-of-type"
+        );
+        if (nextItem) {
+          nextItem.classList.add("scrolled");
+          nextItem.scrollIntoView({ behavior: "smooth", block: "center" });
+        }
+      }
+    });
+});
